a8a9515fb37d226cf6922198fe1b51911ebf98ab
Anastassia2312 <23122000na@gmail.com>
2024-03-20 20:10:46 +0200

    js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const modalWindow = document.querySelector('.backdrop');
+const openModalBtn = document.querySelector('.hero-button');
+const closeModalBtn = document.querySelector('.close-btn');
+const closeMobileMenuBtn = document.querySelector('.mobile-close-btn');
+const openMobileMenuBtn = document.querySelector('.open-mobile-menu-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+openModalBtn.addEventListener('click', () => {
+  modalWindow.classList.add('is-open');
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modalWindow.classList.remove('is-open');
+});
+
+openMobileMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('.is-open');
+});
+
+closeMobileMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('.is-open');
+});
